f63d21592db500f0ea92366e4e1bc9e4c6df682b
Slash663 <31983253+Slash663@users.noreply.github.com>
2022-08-18 09:01:28 +0200

    Update RegistrationConfirmedAction.php
    Since symfony/security-core 5.2: Method "Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken::getProviderKey" is deprecated, use "getFirewallName()" instead.

--- src/Action/RegistrationConfirmedAction.php
+++ src/Action/RegistrationConfirmedAction.php
@@ -58,11 +58,11 @@ final class RegistrationConfirmedAction
     {
         $token = $this->tokenStorage->getToken();
 
-        if (null === $token || !\is_callable([$token, 'getProviderKey'])) {
+        if (null === $token || !\is_callable([$token, 'getFirewallName'])) {
             return null;
         }
 
-        $key = sprintf('_security.%s.target_path', $token->getProviderKey());
+        $key = sprintf('_security.%s.target_path', $token->getFirewallName());
 
         if ($session->has($key)) {
             return $session->get($key);
